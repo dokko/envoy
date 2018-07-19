0f2198455163ac979146cfe2dfe4256da819b934
jongwon lee <imadokko@gmail.com>
2018-07-20 03:28:12 +0700

    change to php7.2

--- scripts/serve.sh
+++ scripts/serve.sh
@@ -16,7 +16,7 @@
 #   (We assume username you are going to use is 'deployer')
 #
 #   deployer ALL=(ALL:ALL) NOPASSWD: ALL
-#   %www-data ALL=(ALL:ALL) NOPASSWD:/usr/sbin/service php7.0-fpm restart,/usr/sbin/service nginx restart
+#   %www-data ALL=(ALL:ALL) NOPASSWD:/usr/sbin/service php7.2-fpm restart,/usr/sbin/service nginx restart
 ##
 
 ##
@@ -58,7 +58,7 @@ block="server {
 
     location ~ \.php$ {
         fastcgi_split_path_info ^(.+\.php)(/.+)$;
-        fastcgi_pass unix:/var/run/php/php7.0-fpm.sock;
+        fastcgi_pass unix:/var/run/php/php7.2-fpm.sock;
         fastcgi_index index.php;
         include fastcgi_params;
         fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
@@ -80,4 +80,4 @@ echo "$block" > "/etc/nginx/sites-available/$1"
 ln -fs "/etc/nginx/sites-available/$1" "/etc/nginx/sites-enabled/$1"
 
 sudo service nginx restart
-sudo service php7.0-fpm restart
+sudo service php7.2-fpm restart
