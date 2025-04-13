3a6e13b7e5c292d0702fd95cc7ae5661a6fd56ff
jongwon lee <imadokko@gmail.com>
2025-04-13 23:25:32 +0900

    Update provision8.4.sh

--- scripts/provision8.4.sh
+++ scripts/provision8.4.sh
@@ -32,6 +32,10 @@
 ##
 # House keeping: makes script not running without proper arguments.
 ##
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
 
 if [[ -z "$1" ]]
 then
